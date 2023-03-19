f7201adc70e5fbd9ac33aaadd888cc36f84d924e
Raymond <raymond.luke.spartz@gmail.com>
2023-03-19 11:59:06 -0700

    added ReactScene from spline into Library

---
+++ library/app/components/ReactScene.tsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/noBInCq8QAwEf3n0/scene.splinecode" />
+  );
+}
