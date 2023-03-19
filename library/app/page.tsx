eb1c5e6d78dc678d5c887d73ac7a09dbd44189ad
Raymond <raymond.luke.spartz@gmail.com>
2023-03-19 14:22:41 -0700

    added Logo.tsx and added to home

--- library/app/page.tsx
+++ library/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
 import dynamic from "next/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,15 +11,28 @@ const Stars = dynamic<{}>(
   { ssr: false }
 );
 
+const Logo = dynamic<{}>(
+  () => import("../app/components/Logo").then((mod) => mod.default),
+  { ssr: false }
+);
+
 export default function Home() {
   return (
-    <main>
-      <div className="absolute w-screen h-screen z-0">
+    <main className="bg-black">
+      <div className="fixed w-screen h-screen z-0">
         {" "}
         <Stars />
       </div>
-      <div className=" flex flex-vertical bg-black w-screen h-auto ">
-        <h1 className="text-4xl font-mono uppercase">Home</h1>
+      <div className="flex w-screen h-screen relative">
+        <div className="w-full h-full absolute z-0">
+          <Logo />
+        </div>
+      </div>
+      <div className="flex w-screen h-screen justify-center items-center ">
+        <div className="flex bg-gray-600 w-4/5 h-4/5 relative z-10"></div>
+      </div>
+      <div className="flex w-screen h-screen justify-center items-center ">
+        <div className="flex bg-gray-400 w-4/5 h-4/5 relative z-10"></div>
       </div>
     </main>
   );
