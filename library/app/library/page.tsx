633402ad6e30028476ea5aec69cb0f8938ededf9
Raymond <raymond.luke.spartz@gmail.com>
2023-03-19 11:58:47 -0700

    next/babel

--- library/app/library/page.tsx
+++ library/app/library/page.tsx
@@ -1,7 +1,28 @@
-export default function Library() {
+"use client";
+
+import Head from "next/head";
+import dynamic from "next/dynamic";
+
+const ReactScene = dynamic<{}>(
+  () => import("../components/ReactScene").then((mod) => mod.default),
+  { ssr: false }
+);
+
+export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-mono uppercase">Library</h1>
-    </div>
+    <>
+      <Head>
+        <title>Library</title>
+        <meta name="description" content="My website description"></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="flex w-screen h-screen bg-black relative">
+          <div className="w-full h-full absolute">
+            <ReactScene />
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
